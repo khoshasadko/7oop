7192dda55ef3a3939db8dcc43ea093ebad53dedc
khoshasadko <104422209+khoshasadko@users.noreply.github.com>
2022-06-10 22:28:09 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "oop7.h"
+
+int main() {
+    Circle a(2, 3, 6);
+    Circle b(a);
+}
---
+++ oop7.h
@@ -0,0 +1,35 @@
+#ifndef OOP7_H
+#define OOP7_H
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Circle {
+    double x;
+    double y;
+    double r;
+
+public:
+    Circle(double xx = 0, double yy = 0, double rr = 0) {
+        x = xx;
+        y = yy;
+        r = rr;
+        cout << this->x << this->y << this->r << endl;
+    };
+
+    Circle(const Circle& obj) {
+        cout << this->x << this->y << this->r << endl;
+        this->x = obj.x;
+        this->y = obj.y;
+        this->r = obj.r;
+        cout << obj.x << obj.y << obj.r << endl;
+    };
+
+    ~Circle() {
+        cout << this << " destr" << endl;
+    };
+};
+
+#endif 
